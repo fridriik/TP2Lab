72128b9a80e155f57b8ddc62025b83f47eeed3b0
Federico <federicofarias@outlook.com>
2024-09-04 02:13:05 -0300

    clean EmpleadoDTO

--- src/main/java/com/turnosrotativos/dto/EmpleadoDTO.java
+++ src/main/java/com/turnosrotativos/dto/EmpleadoDTO.java
@@ -56,57 +56,33 @@ public class EmpleadoDTO {
         this.id = id;
     }
 
-    public @NotBlank(message = "El nombre es obligatorio.") @Pattern(regexp = "^[a-zA-Z]+$", message = "Solo se permiten letras en el campo 'nombre'.") String getNombre() {
-        return nombre;
-    }
+    public String getNombre() {return nombre;}
 
-    public void setNombre(@NotBlank(message = "El nombre es obligatorio.") @Pattern(regexp = "^[a-zA-Z]+$", message = "Solo se permiten letras en el campo 'nombre'.") String nombre) {
-        this.nombre = nombre;
-    }
+    public void setNombre(String nombre) {this.nombre = nombre;}
 
-    public @NotBlank(message = "El apellido es obligatorio.") @Pattern(regexp = "^[a-zA-Z]+$", message = "Solo se permiten letras en el campo 'apellido'.") String getApellido() {
-        return apellido;
-    }
+    public String getApellido() {return apellido;}
 
-    public void setApellido(@NotBlank(message = "El apellido es obligatorio.") @Pattern(regexp = "^[a-zA-Z]+$", message = "Solo se permiten letras en el campo 'apellido'.") String apellido) {
-        this.apellido = apellido;
-    }
+    public void setApellido(String apellido) {this.apellido = apellido;}
 
-    public @NotBlank(message = "El email es obligatorio.") @Email(message = "El email ingresado no es correcto.") String getEmail() {
-        return email;
-    }
+    public String getEmail() {return email;}
 
-    public void setEmail(@NotBlank(message = "El email es obligatorio.") @Email(message = "El email ingresado no es correcto.") String email) {
-        this.email = email;
-    }
+    public void setEmail(String email) {this.email = email;}
 
-    public @NotNull(message = "El número de documento es obligatorio.") @DecimalMin(value = "1000000", message = "El número de documento debe tener al menos 7 dígitos.") @DecimalMax(value = "99999999", message = "El número de documento no puede tener más de 8 dígitos.") Integer getNroDocumento() {
-        return nroDocumento;
-    }
+    public Integer getNroDocumento() {return nroDocumento;}
 
-    public void setNroDocumento(@NotNull(message = "El número de documento es obligatorio.") @DecimalMin(value = "1000000", message = "El número de documento debe tener al menos 7 dígitos.") @DecimalMax(value = "99999999", message = "El número de documento no puede tener más de 8 dígitos.") Integer nroDocumento) {
-        this.nroDocumento = nroDocumento;
-    }
+    public void setNroDocumento(Integer nroDocumento) {this.nroDocumento = nroDocumento;}
 
-    public @NotNull(message = "La fecha de nacimiento es obligatoria.") LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
+    public LocalDate getFechaNacimiento() {return fechaNacimiento;}
 
-    public void setFechaNacimiento(@NotNull(message = "La fecha de nacimiento es obligatoria.") LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {this.fechaNacimiento = fechaNacimiento;}
 
     public LocalDate getFechaIngreso() {
         return fechaIngreso;
     }
 
-    public void setFechaIngreso(@NotNull(message = "La fecha de ingreso es obligatoria.") @Past(message = "La fecha de ingreso no puede ser posterior al día de la fecha.") LocalDate fechaIngreso) {
-        this.fechaIngreso = fechaIngreso;
-    }
+    public void setFechaIngreso(LocalDate fechaIngreso) {this.fechaIngreso = fechaIngreso;}
 
-    public LocalDateTime getFechaCreacion() {
-        return fechaCreacion;
-    }
+    public LocalDateTime getFechaCreacion() {return fechaCreacion;}
 
     public void setFechaCreacion(LocalDateTime fechaCreacion) {
         this.fechaCreacion = fechaCreacion;
